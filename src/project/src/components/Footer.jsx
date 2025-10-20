4273b6e8e3cda2417d63f1f7a83a9e0a61fa9a68
Dennis Vera <dennis.vera.u@gmail.com>
2025-10-20 19:10:23 -0400

    Boostrap 5 Version

--- src/project/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-dark text-light text-center py-3 mt-5">
-      <p className="mb-0">© {new Date().getFullYear()} Mi Landing Page. Todos los derechos reservados.</p>
-    </footer>
-  );
-}
-
-export default Footer;
